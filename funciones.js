d4930553c28ddb90f771f692c983fd0b196109d3
lucasperedo <115240272+lucasperedo@users.noreply.github.com>
2023-01-25 14:03:26 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,11 @@
+document.getElementById("boton1").onclick = function () {
+    document.getElementById("telefono").innerHTML = "351 2120755"
+}
+
+document.getElementById("boton2").onclick = function () {
+    document.getElementById("domicilio").innerHTML = "Tránsito Cáceres de Allende 386 6D"
+}
+
+document.getElementById("boton3").onclick = function () {
+    document.getElementById("email").innerHTML = "lperedo2002@gmail.com"
+}
